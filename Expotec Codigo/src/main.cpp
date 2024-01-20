a138ae02e02fb51ddf62f1e501d58fb0950eb2de
Tinex3 <b.riquelme.gomez@gmail.com>
2024-01-19 23:33:05 -0300

    Se agrega extructura de datos

--- Expotec Codigo/src/main.cpp
+++ Expotec Codigo/src/main.cpp
@@ -8,13 +8,32 @@
 #define TIEMPO_ACCION 5000 // Tiempo de ejecución de la acción en milisegundos (por ejemplo, 5 segundos)
 #define INTERVALO_LECTURA 500 // Intervalo de lectura en milisegundos (150 ms en este caso)
 
-#define LM35 A0
-#define EN_TIERRA A1
 
 
-#define BOMBA 3
-#define VENTILADOR1 4
-#define VENTILADOR2 5
+typedef struct Controldeinvernadero{
+    uint16_t LM35;
+    uint16_t Lectura_En_Tierra;
+    uint16_t BOMBA;
+    uint16_t VENTILADOR1;
+    uint16_t VENTILADOR2;
+}Control_Agua;
+Control_Agua Maqueta;
+
+
+
+typedef struct Datos
+{
+    uint16_t LecturaLM35;
+    uint16_t ValorTierra;
+    uint16_t HumedadDHT;
+    uint16_t TemperaturaDHT;
+
+}Lecturas;
+Lecturas Sensores;
+
+
+
+
 
 DHT dht(DHTPIN, DHTTYPE);
 
@@ -27,22 +46,29 @@ Estado estado = ESPERANDO_COMANDO;
 unsigned long tiempoInicioAccion = 0;
 unsigned long tiempoUltimaLectura = 0;
 
-void lecturadedatos();
+void lecturadedatos(Lecturas *Sensores,Control_Agua *Perifericos);
 void ejecutarComando(char comando);
-void activarBomba();
-void activarVentilador1();
-void activarVentilador2();
-void detenerSalidas();
+void activarBomba(Control_Agua *Maqueta);
+void activarVentilador1(Control_Agua &Perifericos);
+void activarVentilador2(Control_Agua &Perifericos);
+void detenerSalidas(Control_Agua &Perifericos);
+void Init_maqueta(Control_Agua &Perifericos);
+void Imprimir(Lecturas *Sensores);
 
 
 void setup() {
     Serial.begin(BAUDIOS);
     dht.begin();
-    pinMode(LM35, INPUT);
-    pinMode(EN_TIERRA, INPUT);
-    pinMode(BOMBA, OUTPUT);
-    pinMode(VENTILADOR1, OUTPUT);
-    pinMode(VENTILADOR2, OUTPUT);
+    Init_maqueta(Maqueta);
+
+}
+
+void Init_maqueta(Control_Agua &Perifericos){
+    pinMode(Perifericos.LM35,INPUT);
+    pinMode(Perifericos.Lectura_En_Tierra, INPUT);
+    pinMode(Perifericos.BOMBA, OUTPUT);
+    pinMode(Perifericos.VENTILADOR1,OUTPUT);
+    pinMode(Perifericos.VENTILADOR2,OUTPUT);
 }
 
 void loop() {
@@ -50,7 +76,8 @@ void loop() {
 
     // Realizar lectura y enviar datos cada 150 ms
     if (tiempoActual - tiempoUltimaLectura >= INTERVALO_LECTURA) {
-        lecturadedatos();
+        lecturadedatos( &Sensores,&Maqueta);
+        Imprimir(&Sensores);
         tiempoUltimaLectura = tiempoActual;
     }
 
@@ -63,7 +90,7 @@ void loop() {
             break;
         case EJECUTANDO_ACCION:
             if (millis() - tiempoInicioAccion >= TIEMPO_ACCION) {
-                detenerSalidas();
+                detenerSalidas(Maqueta);
                 estado = ESPERANDO_COMANDO;
             }
             break;
@@ -73,65 +100,65 @@ void loop() {
 void ejecutarComando(char comando) {
     switch (comando) {
         case 'A':
-            activarBomba();
+            activarBomba(&Maqueta);
             break;
         case 'B':
-            activarVentilador1();
+            activarVentilador1(Maqueta);
             break;
         case 'C':
-            activarVentilador2();
+            activarVentilador2(Maqueta);
             break;
         case 'D':
-            activarBomba();
-            activarVentilador1();
-            activarVentilador2();
+            activarBomba(Maqueta);
+            activarVentilador1(Maqueta);
+            activarVentilador2(Maqueta);
             break;
         // Agrega más casos de comandos si es necesario
     }
 }
 
-void activarBomba() {
-    digitalWrite(BOMBA, HIGH);
+void activarBomba(Control_Agua &Perifericos){
+    digitalWrite(Perifericos.BOMBA, HIGH);
     tiempoInicioAccion = millis();
     estado = EJECUTANDO_ACCION;
 }
 
-void activarVentilador1() {
-    digitalWrite(VENTILADOR1, HIGH);
+void activarVentilador1(Control_Agua &Perifericos) {
+    digitalWrite(Perifericos.VENTILADOR1, HIGH);
     tiempoInicioAccion = millis();
     estado = EJECUTANDO_ACCION;
 }
 
-void activarVentilador2() {
-    digitalWrite(VENTILADOR2, HIGH);
+void activarVentilador2(Control_Agua &Perifericos) {
+    digitalWrite(Perifericos.VENTILADOR2, HIGH);
     tiempoInicioAccion = millis();
     estado = EJECUTANDO_ACCION;
 }
 
-void detenerSalidas() {
-    digitalWrite(BOMBA, LOW);
-    digitalWrite(VENTILADOR1, LOW);
-    digitalWrite(VENTILADOR2, LOW);
+void detenerSalidas(Control_Agua &Perifericos) {
+    digitalWrite(Perifericos.BOMBA, LOW);
+    digitalWrite(Perifericos.VENTILADOR1, LOW);
+    digitalWrite(Perifericos.VENTILADOR2, LOW);
 }
 
-void lecturadedatos() {
+void lecturadedatos(Lecturas *Sensores, Control_Agua *Maqueta) {
     // Lógica para leer datos de los sensores y enviarlos por el puerto serie
-    int valorLM35 = analogRead(LM35);
-    int temperaturaLM35 = 10000+((5*valorLM35*100)/1024); // Convertir el valor a temperatura en grados Celsius
+    int valorLM35 = analogRead(Maqueta->LM35);
+    Sensores->LecturaLM35 = 10000+((5*valorLM35*100)/1024); // Convertir el valor a temperatura en grados Celsius
 
-    int valorTierra = 10000+analogRead(EN_TIERRA);
-
-    int humedad = 10000+dht.readHumidity();
-    int temperaturaDHT = 10000+dht.readTemperature();
-
+    int valorTierra = 10000+analogRead(Maqueta->Lectura_En_Tierra);
+    Sensores->ValorTierra = valorTierra;
+    Sensores->HumedadDHT = 10000+dht.readHumidity();
+    Sensores->TemperaturaDHT = 10000+dht.readTemperature();
+}
+void Imprimir(Lecturas *Sensores){
     // Enviar datos por Serial (puedes modificar este formato según tus necesidades)
-    Serial.print(valorTierra);
+    Serial.print(Sensores->ValorTierra);
     Serial.print("\t");
-    Serial.print(temperaturaLM35);
+    Serial.print(Sensores->LecturaLM35);
     Serial.print("\t");
-    Serial.print(humedad);
+    Serial.print(Sensores->HumedadDHT);
     Serial.print("\t");
-    Serial.print(temperaturaDHT);
+    Serial.print(Sensores->TemperaturaDHT);
     Serial.print("\t"); 
-    
 }
